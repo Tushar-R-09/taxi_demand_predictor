7cbd3c5e95e3e543a30090497a0e32c45c2fdcd7
Tushar Rohilla <tusharrohilla70#gmail.com>
2024-12-29 10:28:21 +0530

    Feature pipeline

---
+++ src/config.py
@@ -0,0 +1,17 @@
+import os
+from dotenv import load_dotenv
+from src.paths import PARENT_DIR
+
+load_dotenv(PARENT_DIR / ".env")
+
+try:
+    HOPSWORK_API_KEY = os.environ["HOPSWORK_API_KEY"]
+
+except:
+    raise Exception(
+        "Please set HOPSWORK_API_KEY in your .env file."
+    )
+HOPSWORK_PROJECT_NAME = "taxidemand"
+
+FEATURE_GROUP_NAME = "time_series_hourly_feature_group"
+FEATURE_GROUP_VERSION = 1
